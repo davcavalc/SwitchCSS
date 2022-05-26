750e94743899aa55b86b557d610cea00bd21a345
Davner <davner23@gmail.com>
2022-05-26 03:11:19 -0400

    Atualização 26/05/2022

--- checkbox.js
+++ checkbox.js
@@ -1,8 +1,10 @@
-let checkbox = checkbox.addEventListener("click", (event) => {
-  if (checkbox.classList.contains("on")) {
-    checkbox.setAttribute("aria-checked", "false");
-  } else {
-    checkbox.setAttribute("aria-checked", "true");
-  }
-  checkbox.classList.toggle("on");
-});
+const checkbox = document
+  .querySelector("aria-checked")
+  .addEventListener("click", (event) => {
+    if (checkbox.classList.contains("on")) {
+      checkbox.setAttribute("aria-checked", "false");
+    } else {
+      checkbox.setAttribute("aria-checked", "true");
+    }
+    checkbox.classList.toggle("on");
+  });
